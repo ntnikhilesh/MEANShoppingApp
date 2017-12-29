044871f51244b550328a34c088a77668969c863a
ntnikhilesh <ntnikhilesh@gmail.com>
2017-12-29 13:23:05 +0530

    add_item and get items done

--- crud-backend/route/routes.js
+++ crud-backend/route/routes.js
@@ -1,9 +1,44 @@
 var express=require('express');
 var router=express.Router();
 
+const Item=require('../model/shoppingItem')
+
 //retreiving data from DB
-router.get('/testing_get_route',(req,res,next)=>{
-    res.send('get route tested');
+router.get('/get_items',(req,res,next)=>{
+   
+    Item.find(function(err,items){
+        if(err){
+            res.json(err);
+        }else{
+            res.json(items);
+        }
+    })
+})
+
+//inserting new data
+router.post('/add_items',(req,res,next)=>{
+    let newShoppingItem=new Item({
+        itemName:req.body.itemName,
+        itemQuantity:req.body.itemQuantity,
+        itemBought:req.body.itemBought
+    });
+    newShoppingItem.save((err,item)=>{
+        if(err){
+            res.json(err);
+        }else{
+            res.json({msg:'Item has been added successfully'});
+        }
+    });
+});
+
+//updating the data
+router.put('/put_route',(req,res,next)=>{
+    //to do later
+})
+
+//delete the data
+router.get('/delete_route',(req,res,next)=>{
+     //to do later
 })
 
 module.exports=router;
